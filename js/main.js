2cf2186191cfe7b0a782f7ba8546e0985d20abdc
Ilya <112574854+skrimza@users.noreply.github.com>
2022-12-26 18:02:18 +0300

    commit the scrolling content

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 5000,
+    },
+    slidesPerView: "1",
+    direction: 'horizontal',
+    loop: true,
+    allowTouchMove: false,
+    effect: 'fade',
+    spaceBetween: 50,
+    speed: 800,
+});
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active');
+            } else {
+                if (!animItem.classList.contains('anim-no-hide')) {
+                    animItem.classList.remove('active');
+                }
+            }
+        }
+
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top scrollTop, left: rect.left + scrollLeft }
+
+    }
+    animOnScroll();
+}
